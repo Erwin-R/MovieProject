8a8c79a4632b555061a95aa881958e66827bbb8f
Eloy Vigil <112999750+EloyVigil@users.noreply.github.com>
2022-12-16 21:07:15 -0800

    Couple changes

---
+++ src/main/java/com/erwinr/movieproject/Services/EmailService.java
@@ -0,0 +1,28 @@
+package com.erwinr.movieproject.Services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+	@Autowired
+	private JavaMailSender emailSender;
+	
+	public EmailService(JavaMailSender emailSender) {
+		this.emailSender = emailSender;
+	}
+	
+	public void sendMessage(String to, String subject, String text) {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setFrom("buefordvigil@gmail.com");
+		message.setTo(to);
+		message.setSubject(subject);
+		message.setText(text);
+		emailSender.send(message);
+		System.out.println("It Works!");
+	}
+	
+}
